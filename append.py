38afcdb3c6fc90031033d89d8514914377820482
Andrew Hammond <ahammond@anchorfree.com>
2018-06-14 12:32:29 -0700

    extend on solutions

--- append.py
+++ append.py
@@ -5,7 +5,7 @@
 def append_to(element, to=[]):
     """
     Append an element to a list.
-    If not list provided, create a new list containing only the element.
+    If no list is provided, create a new list containing only the element.
 
     :param element: the thing to append
     :param to: the list to append it to
@@ -15,22 +15,65 @@ def append_to(element, to=[]):
     to.append(element)
     return to
 
-# Do not edit below here.
-import unittest
+extra_credit = 0
+
+# DO NOT CHANGE ANYTHING BELOW THIS LINE
+# Test section. All these tests should pass when run as follows.
+#
+# After you have solved the initial problem,
+# if you want to go for extra credit,
+# please to set the extra_credit to 1, or 2
+# Then read the tests to see what is expected.
+
+import mock, unittest
 class TestAppendTo(unittest.TestCase):
-    def test_default(self):
-        r = append_to('a')
-        self.assertListEqual(r, ['a'])
 
-    def test_no_default(self):
-        l = ['a', 'b']
-        r = append_to('c', l)
-        self.assertListEqual(r, ['a', 'b', 'c'])
+    def setUp(self):
+        self._input_list = [mock.sentinel.first, mock.sentinel.second, mock.sentinel.third]
 
-    def test_both(self):
-        r = append_to('b')
-        append_to('c', r)
-        self.assertListEqual(r, ['b', 'c'])
+    @property
+    def input_list(self):
+        return list(self._input_list)
+
+    def test_returns_an_array(self):
+        'The method should return a list'
+        r = append_to(mock.sentinel.test_returns_an_array, self.input_list)
+        self.assertIsInstance(r, list)
+
+    def test_returns_same_array_it_was_given(self):
+        'The method should modify the input_list in place'
+        input_list_copy = self.input_list
+        r = append_to(mock.sentinel.junk, input_list_copy)
+        self.assertIs(r, input_list_copy)
+
+    def test_append_value_is_last_entry(self):
+        'The value passed should be added to the end of the list.'
+        v = mock.sentinel.should_be_last_entry
+        r = append_to(v, self.input_list)
+        self.assertIs(r[-1], v)
+
+    def test_appending_value_to_default_generates_new_list(self):
+        'Distinct calls should not have any side-effects.'
+        r1 = append_to(mock.sentinel.first_run)
+        r2 = append_to(mock.sentinel.second_run)
+        self.assertListEqual(r1, [mock.sentinel.first_run])
+        self.assertListEqual(r2, [mock.sentinel.second_run])
+
+    @unittest.skipIf(extra_credit < 1, 'Not trying for 1st tier of extra credit')
+    def test_scalarization_of_list(self):
+        'If thing is not a scalar, flatten it into a scalar'
+        v = [mock.sentinel.a, mock.sentinel.b]
+        r = append_to(v, self.input_list)
+        self.assertListEqual(r, [mock.sentinel.first, mock.sentinel.second, mock.sentinel.third,
+                                 mock.sentinel.a, mock.sentinel.b])
+
+    @unittest.skipIf(extra_credit < 2, 'Not trying for 2nd tier of extra credit')
+    def test_scalarization_of_dict(self):
+        'If thing is not a scalar, flatten it into a scalar'
+        v = {mock.sentinel.a: mock.sentinel.b}
+        r = append_to(v, self.input_list)
+        self.assertListEqual(r, [mock.sentinel.first, mock.sentinel.second, mock.sentinel.third,
+                                 mock.sentinel.a])
 
 
 unittest.main(exit=False)
--- append_solution.py
+++ append_solution.py
@@ -6,6 +6,60 @@ def append_to(element, to=None):
     to.append(element)
     return to
 
-"""
-Expect an explanation around compile time instantiation vs runtime.
-"""
+# Expect an explanation around compile time instantiation vs runtime.
+
+# Next step is to have the candidate work through the UTs and explain how they work.
+# Do they understand the concept of mocks?
+
+# Extra credit 1: flatten list and extend
+# candidate should describe what the test expects,
+# figure out that it requires them to flatten the element by effectively extending the list.
+
+def append_to(element, to=None):
+    if to is None:      # test if None
+        to = []
+    if isinstance(element, list):
+        to.extend(element)
+    else:
+        to.append(element)
+    return to
+
+
+# Extra credit 2: flatten dict and extend
+
+def append_to(element, to=None):
+    if to is None:      # test if None
+        to = []
+    if isinstance(element, (tuple, list)):
+        to.extend(element)
+    elif isinstance(element, dict):
+        to.extend(list(element))
+    else:
+        to.append(element)
+    return to
+
+
+# General solutions: candidate actually knows python really well.
+
+def append_to(element, to=None):
+    if to is None:      # test if None
+        to = []
+    try:
+        for i in element:
+            to.append(i)
+    except:
+        to.append(element)
+    return to
+
+
+# The best solution, because it doesn't require slow, expensive exception handling.
+
+import collections
+def append_to(element, to=None):
+    if to is None:      # test if None
+        to = []
+    if isinstance(element, collections.Iterable):
+        to.extend(list(element))
+    else:
+        to.append(element)
+    return to
